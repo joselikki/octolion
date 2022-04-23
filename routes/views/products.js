3b2b92a63d16583578e02442f6a72bbb78e6cb8e
Jose Paredes <jose@softquin.com.gt>
2022-04-22 23:52:13 -0600

    Mockup data fixed

--- routes/views/products.js
+++ routes/views/products.js
@@ -11,12 +11,14 @@ const cacheResponse = require('../../utils/cacheResponse')
 const { FIVE_MINUTES_IN_SECONDS } = require('../../utils/time')
 
 
+const products = require('../../utils/mocks/products')
+
 //GET PRODUCTS IN HOMEPAGE
 router.get('/', async (req, res, next)=>{
     cacheResponse(res, FIVE_MINUTES_IN_SECONDS)
     const { tags } = req.query
     try {
-        const  products = await productService.getProducts({ tags })
+        //const  products = await productService.getProducts({ tags })
         res.render("products", { products, dev: config.dev })
         
     } catch (error) {
